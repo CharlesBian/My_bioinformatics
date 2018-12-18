3420348c7784e1be23163a2af4eb743ff1af4f0a
Charlesbian_computer <charlesbian888@gmail.com>
2018-12-18 22:59:21 +0800

    firehose phenotype

--- firehose_phenotype.R
+++ firehose_phenotype.R
@@ -12,9 +12,22 @@ all_tumor_countdata <- read.csv(choose.files())
 pair_normal_countdata <- read.csv(choose.files())
 pair_tumor_countdata <- read.csv(choose.files())
 
+b <- t(all_tumor_countdata[-1])
+colnames(b)<-all_tumor_countdata$geneID
+
 pheno <- read.csv(choose.files(),sep = '\t', header = TRUE)
+c<-t(pheno[-1])
+colnames(c) <- pheno$Hybridization.REF
+
+
+name_tumor <- colnames(all_tumor_countdata[-1])
+a <- substr(name_tumor,1,12)
+a <-  c("GeneID",a)
+
+
 name <- colnames(pheno[-1])
 
+finally <- full_join()
 
 
 
