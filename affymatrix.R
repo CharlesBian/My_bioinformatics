79cfc9638fc0f688192e6bc134937f644ca98bea
Charlesbian_computer <charlesbian888@gmail.com>
2018-12-19 22:08:33 +0800

    affy

--- affymatrix.R
+++ affymatrix.R
@@ -1,7 +1,12 @@
-library(affy)
+library("annaffy")
+library("limma")
+library("affy")
+library("Biobase")
+
 dir_cels = ''
 affy_data = ReadAffy(celfile.path = dir_cels)
 eset.mas5 = mas5(affy_data)
+
 
 
 
--- firehose_phenotype.R
+++ firehose_phenotype.R
@@ -7,20 +7,24 @@ library(dplyr)
 setwd(choose.dir())
 getwd()
 
-all_normal_countdata <- read.csv(choose.files())
 all_tumor_countdata <- read.csv(choose.files())
 pair_normal_countdata <- read.csv(choose.files())
 pair_tumor_countdata <- read.csv(choose.files())
 
-b <- as.data.frame(t(all_tumor_countdata[-1]))
-colnames(b)<-all_tumor_countdata$geneID
+countdata <- all_normal_countdata
+countdata <- all_tumor_countdata
+countdata <- pair_normal_countdata
+countdata <- pair_tumor_countdata
+
+b <- as.data.frame(t(countdata[-1]))
+colnames(b)<-countdata$geneID
 
 pheno <- read.csv(choose.files(),sep = '\t', header = TRUE)
 c<-as.data.frame(t(pheno[-1]))
 colnames(c) <- pheno$Hybridization.REF
 c$sample <-toupper(colnames(pheno[-1]))
 
-name_tumor <- colnames(all_tumor_countdata[-1])
+name_tumor <- colnames(countdata[-1])
 a <- substr(name_tumor,1,12)
 #a <-  c("GeneID",a)
 b$sample <- a
@@ -29,14 +33,39 @@ b <- b[!duplicated(b[,20532]),]
 finally <- inner_join(c,b,by =c("sample"="sample"))
 rownames(finally)<-finally$sample
 finally3 <- filter(finally,days_to_last_followup ==0)
+rownames(finally3)<-finally3$sample
 
 finally <- as.data.frame(t(finally))
-colnames(finally3)
 finally3 <- as.data.frame(t(finally3))
 
-finally2 <- subset(finally,selected = setdiff(finally$sample,finally3$sample))
-write.csv(finally,"all_tumor_coadread.csv",
+finally2 <- subset(finally,select = setdiff(colnames(finally),colnames(finally3)))
+
+write.csv(finally2[-20,],"pair_tumor_coadread.csv",
            row.names = TRUE)
+
+
+#survival analysis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
