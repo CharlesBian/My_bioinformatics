b7eb844041f40d50d3a508c28121b0e3536da09d
Charlesbian_computer <charlesbian888@gmail.com>
2018-12-19 20:47:54 +0800

    firehose phenotype

--- GEO_analysis.R
+++ GEO_analysis.R
@@ -1,7 +1,53 @@
+#   Differential expression analysis with limma
 library("GEOquery")
 library("annaffy")
 library("limma")
 library("affy")
+library("Biobase")
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE46495", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL11533", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+# group names for all samples
+gsms <- "000001111122222333334444455555"
+sml <- c()
+for (i in 1:nchar(gsms)) { sml[i] <- substr(gsms,i,i) }
+
+# log2 transform
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+  (qx[6]-qx[1] > 50 && qx[2] > 0) ||
+  (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+# set up the data and proceed with analysis
+sml <- paste("G", sml, sep="")    # set group names
+fl <- as.factor(sml)
+gset$description <- fl
+design <- model.matrix(~ description + 0, gset)
+colnames(design) <- levels(fl)
+fit <- lmFit(gset, design)
+cont.matrix <- makeContrasts(G5-G0, G1-G0, G2-G1, G3-G2, G4-G3, G5-G4, levels=design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2, 0.01)
+tT <- topTable(fit2, adjust="fdr", sort.by="B",number = "IF")
+
+tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","F","Gene.symbol","Gene.title"))
+write.table(tT, file=stdout(), row.names=F, sep="\t")
+
+
+
+
+
+
 
 
 
--- firehose_phenotype.R
+++ firehose_phenotype.R
@@ -12,24 +12,31 @@ all_tumor_countdata <- read.csv(choose.files())
 pair_normal_countdata <- read.csv(choose.files())
 pair_tumor_countdata <- read.csv(choose.files())
 
-b <- t(all_tumor_countdata[-1])
+b <- as.data.frame(t(all_tumor_countdata[-1]))
 colnames(b)<-all_tumor_countdata$geneID
 
 pheno <- read.csv(choose.files(),sep = '\t', header = TRUE)
-c<-t(pheno[-1])
+c<-as.data.frame(t(pheno[-1]))
 colnames(c) <- pheno$Hybridization.REF
-
+c$sample <-toupper(colnames(pheno[-1]))
 
 name_tumor <- colnames(all_tumor_countdata[-1])
 a <- substr(name_tumor,1,12)
-a <-  c("GeneID",a)
+#a <-  c("GeneID",a)
+b$sample <- a
+b <- b[!duplicated(b[,20532]),]
 
+finally <- inner_join(c,b,by =c("sample"="sample"))
+rownames(finally)<-finally$sample
+finally3 <- filter(finally,days_to_last_followup ==0)
 
-name <- colnames(pheno[-1])
+finally <- as.data.frame(t(finally))
+colnames(finally3)
+finally3 <- as.data.frame(t(finally3))
 
-finally <- full_join()
-
-
+finally2 <- subset(finally,selected = setdiff(finally$sample,finally3$sample))
+write.csv(finally,"all_tumor_coadread.csv",
+           row.names = TRUE)
 
 
 
